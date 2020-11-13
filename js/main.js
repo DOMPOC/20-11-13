35837af78377f99f8fed64a209396ec615d5e548
Domas Pocius <domas.pocius@gmail.com>
2020-11-13 14:14:44 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function (){
+    if (xhr.readyState === 4){
+        let mokiniai = JSON.parse(xhr.responseText);
+
+        let sarasas ='<ul>';
+        for (i=0; i<mokiniai.length;i++){
+            if (mokiniai[i].lokacija === true){
+                sarasas +='<li class="yra">';
+
+            }else {
+                sarasas += '<li class="nera">';
+            }
+            sarasas+= mokiniai[i].vardas;
+            sarasas+='</li>';
+        }
+        sarasas+='</ul>';
+        document.querySelector('.mokiniai').innerHTML = sarasas;
+
+        console.log(mokiniai)
+    }
+};
+
+xhr.open('GET', 'duomenys/mokiniai.json');
+
+xhr.send();
